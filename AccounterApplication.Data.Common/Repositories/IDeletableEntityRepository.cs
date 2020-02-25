ce7b48deb595b51d02c7fc45df72adf864f19e4c
Ivan Ivanov <snap4n@gmail.com>
2020-02-25 22:31:11 +0200

    Added Repository Layer

---
+++ AccounterApplication.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace AccounterApplication.Data.Common.Repositories
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        Task<TEntity> GetByIdWithDeletedAsync(params object[] id);
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}

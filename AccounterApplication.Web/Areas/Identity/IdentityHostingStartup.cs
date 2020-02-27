2c59141d0a97be9d44b3760737238b0427d1d737
Ivan Ivanov <snap4n@gmail.com>
2020-02-27 08:20:45 +0200

    Added jquery libs, thems fonts, scaffolded Identity

---
+++ AccounterApplication.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AccounterApplication.Data;
+using AccounterApplication.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AccounterApplication.Web.Areas.Identity.IdentityHostingStartup))]
+namespace AccounterApplication.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

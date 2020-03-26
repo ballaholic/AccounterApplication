000213d53536b6ecf1a6fc2e1d15f433ab86e8b4
Ivan Ivanov <snap4n@gmail.com>
2020-03-26 19:53:33 +0200

    Added popup confirmation message for deletion of Income

--- AccounterApplication.Web/wwwroot/js/custom/monthly-income.js
+++ AccounterApplication.Web/wwwroot/js/custom/monthly-income.js
@@ -13,9 +13,24 @@
     });
 });
 
+$('#deleteIncomeModal').on('show.bs.modal', function (event) {
+
+    // Button that triggered the modal
+    var button = $(event.relatedTarget);
+
+    // Extract info from data-* attributes
+    var incomeId = button.data('incomeid');
+
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this);
+    modal.find('#incomeId-hidden').val(incomeId);
+});
+
 $(document).ready(function ($) {
     $(".btn-delete").click(function () {
-        var id = $(this).data("id");
+        var id = $('#incomeId-hidden').val();
+
         alert("Delete " + id);
     });
 });
